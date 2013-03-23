3667869c3eb0d72d2be5e7bcad7b1ec89639261a
Daniel Schep <dschep@gmail.com>
2013-03-23 18:34:02 -0400

    xscreensaver lock not gnome

--- awesome/config__awesome/rc.lua
+++ awesome/config__awesome/rc.lua
@@ -236,7 +236,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey, "Shift"  }, "Return", function () awful.util.spawn(terminal) end),
-    awful.key({ modkey, "Shift"  }, "l", function () awful.util.spawn("gnome-screensaver-command --lock") end),
+    awful.key({ modkey, "Shift"  }, "l", function () awful.util.spawn("xscreensaver-command --lock") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
